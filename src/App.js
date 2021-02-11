d8cd69ad9221bf33a8b8de90de3fc0a15f5f8a5a
the-chornaya <me@chornaya.com>
2021-02-11 17:15:57 +0000

    add JobsPagination component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from "react";
 import {useFetchJobs} from "./useFetchJobs";
 import {Container} from "react-bootstrap";
 import {Job} from "./components/job/Job";
+import {JobsPagination} from "./components/pagination/JobsPagination";
 
 function App() {
     const [params, setParams] = React.useState({});
-    const [page, setPage] = React.useState({});
+    const [page, setPage] = React.useState(1);
     const {jobs, loading, error} = useFetchJobs(params, page);
 
   return (
     <Container className="my-4">
+        <h1 className="mb-4">GitHub Jobs</h1>
+        <JobsPagination page={page} setPage={setPage} hasNextPage={true}/>
         {loading && <h1>Loading...</h1>}
         {error && <h1>Error. Refresh the page.</h1>}
         {jobs.map(job => {
             return <Job key={job.id} job={job}/>
         })}
+        <JobsPagination page={page} setPage={setPage} hasNextPage={true}/>
     </Container>
   );
 }
